bade97876df9fd16d08d1891b10cd82568a02a90
kiransomase <kiran_somase@yahoo.co.in>
2019-07-14 11:51:32 +0530

    first commit by dev2

--- src/login.java
+++ src/login.java
@@ -4,5 +4,7 @@ public class login {
 	public static void main(String[] args){
 		System.out.println("Hello by Dev1");
 		System.out.println("How are u by Dev1");
+		System.out.println("Hi by Dev2");
+		
 	}
 }
